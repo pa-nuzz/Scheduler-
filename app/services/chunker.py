cc29498cd63433ab0ee9ae28d80065063d221985
“pa-nuzz” <anuj.paudel061@gmail.com>
2026-04-07 17:17:13 +0545

    fix:fixing chunking types

--- app/services/chunker.py
+++ app/services/chunker.py
@@ -3,6 +3,7 @@ from typing import List
 class ChunkerService:
     @staticmethod
     def recursive_chunking(text: str, chunk_size:int=800, chunk_overlap: int =100) -> List[str]:
+#recursive chunking by splitting characters/paragraphs
         chunks = []
         start = 0
         while start < len(text):
@@ -13,7 +14,8 @@ class ChunkerService:
     
 
     @staticmethod
-    def semantic_chunking(text:str)-> list[str]:
+    def semantic_chunking(text:str)-> List[str]:
+#seg chunk spliting by sentence or topic based
         sentences = re.split(r'(?<=[.!?]) +', text)
         group_size = 4
         return [' '.join(sentences[i:i+group_size]) for i in range(0, len(sentences),
