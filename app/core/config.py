d8ad1f8e881d253f170fb13e162243581d836e8e
“pa-nuzz” <anuj.paudel061@gmail.com>
2026-04-07 14:11:00 +0545

    Feat:config for handling api

---
+++ app/core/config.py
@@ -0,0 +1,23 @@
+#this file handles the api keys and settings. using pydantic-settings bc it auto reads the env file
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    #API KEY
+    Gemini_API_KEY: str
+    Groq_API_KEY: str
+
+    #Vector DB (Qdrant)
+    QDRANT_API_KEY: str = "http://localhost:6333"
+    COLLECTION_NAME: str = "palm_docs"
+
+    #Redis
+    REDIS_HOST: str = "redis://localhost:6379"
+
+    #Database
+    DB_HOST: str = "sqlite+aiosqlite:///./palm.db"
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+settings = Settings()
+
