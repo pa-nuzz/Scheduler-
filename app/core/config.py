1ace47f85576541b93add697c69f23c451be50ee
“pa-nuzz” <anuj.paudel061@gmail.com>
2026-04-08 17:18:33 +0545

    update:config update

--- app/core/config.py
+++ app/core/config.py
@@ -1,23 +1,19 @@
-#this file handles the api keys and settings. using pydantic-settings bc it auto reads the env file
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    #API KEY
-    Gemini_API_KEY: str
-    Groq_API_KEY: str
+    GOOGLE_API_KEY: str
+    GROQ_API_KEY: str
 
-    QDRANT_URL: str = "http://localhost:6333"
+    QDRANT_URL: str
     QDRANT_API_KEY: str
-    Collection_NAME: str = "palm_collection"
+    COLLECTION_NAME: str = "palm_collection"
 
-    #Redis
-    REDIS_HOST: str = "redis://localhost:6379"
+    REDIS_URL: str
 
-    #Database
-    DB_HOST: str = "sqlite+aiosqlite:///./palm.db"
+    DATABASE_URL: str = "sqlite+aiosqlite:///./palm.db"
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=".env", case_sensitive=True)
+
 
 settings = Settings()
-
